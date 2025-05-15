efd11908c593a2e36c0d10895121051e70610365
Cyn <sancyn10@github.com>
2025-05-14 22:58:08 -0300

    Creación de la pantalla Login

--- ClubDeportivoApp/Form1.Designer.cs
+++ ClubDeportivoApp/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ClubDeportivoApp
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            txtUsuario = new TextBox();
+            pictureBox1 = new PictureBox();
+            txtPass = new TextBox();
+            btnINGRESAR = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(294, 89);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(228, 23);
+            txtUsuario.TabIndex = 2;
+            txtUsuario.TextChanged += textBox1_TextChanged;
+            txtUsuario.Enter += txtUsuario_Enter;
+            txtUsuario.Leave += txtUsuario_Leave;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(50, 89);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(144, 126);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // txtPass
+            // 
+            txtPass.Location = new Point(294, 158);
+            txtPass.Name = "txtPass";
+            txtPass.Size = new Size(228, 23);
+            txtPass.TabIndex = 4;
+            // 
+            // btnINGRESAR
+            // 
+            btnINGRESAR.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnINGRESAR.Location = new Point(362, 213);
+            btnINGRESAR.Name = "btnINGRESAR";
+            btnINGRESAR.Size = new Size(124, 27);
+            btnINGRESAR.TabIndex = 5;
+            btnINGRESAR.Text = "INGRESAR";
+            btnINGRESAR.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnINGRESAR);
+            Controls.Add(txtPass);
+            Controls.Add(pictureBox1);
+            Controls.Add(txtUsuario);
+            Name = "Login";
+            Text = "Login";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private TextBox txtUsuario;
+        private PictureBox pictureBox1;
+        private TextBox txtPass;
+        private Button btnINGRESAR;
     }
 }
--- ClubDeportivoApp/Form1.cs
+++ ClubDeportivoApp/Form1.cs
@@ -1,10 +1,41 @@
 namespace ClubDeportivoApp
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUsuario_Enter(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "USUARIO")
+            {
+                txtUsuario.Text = "";
+            }
+        }
+
+        private void txtUsuario_Leave(object sender, EventArgs e)
+        {
+            if(txtUsuario.Text == "")
+            {
+                txtUsuario.Text = "USUARIO";
+            }
         }
     }
 }
