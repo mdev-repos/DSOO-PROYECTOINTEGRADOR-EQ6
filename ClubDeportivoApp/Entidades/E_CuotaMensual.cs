b75f22fa1bde958b3a4271d2a2ada17ab9ac6ff0
Guille Benitez <guillehbenitez@gmail.com>
2025-05-16 20:17:37 -0300

    Tarjeta clase No Socio

---
+++ ClubDeportivoApp/Entidades/E_CuotaMensual.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubDeportivoApp.Entidades
+{
+    internal class E_CuotaMensual
+    {
+        public string CodCuota { get; set; }
+        public int NroCuota { get; set; }
+        public DateTime Vencimiento { get; set; }
+        public float ValorMensual { get; set; }
+        public string TipoDePago { get; set; }
+
+    }
+}
