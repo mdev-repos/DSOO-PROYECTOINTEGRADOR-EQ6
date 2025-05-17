800c8c8a472973594f1ab8b2d56c38979104ffdd
Cyn <sancyn10@github.com>
2025-05-16 21:11:33 -0300

    Creación de la entidad socio

---
+++ ClubDeportivoApp/SQL/Tabla Socio.sql
@@ -0,0 +1,11 @@
+﻿use clubdeportivo;
+
+create table Socio(
+	CodSocio varchar(50),
+	DNI int,
+	Carnet boolean,
+	FechaInscripcion varchar(20),
+	Moroso boolean,
+	constraint pk_Socio primary key (CodSocio),
+	constraint fk_Cliente foreign key (DNI) references Cliente(DNI)
+);
