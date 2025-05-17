6d4bb5546cd9595b26468f01e2fa0c16a0cf6202
Cyn <sancyn10@github.com>
2025-05-16 23:48:14 -0300

    Se agregó herencia

--- ClubDeportivoApp/Datos/Socio.cs
+++ ClubDeportivoApp/Datos/Socio.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ClubDeportivoApp.Entidades;
 using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI;
 
 namespace ClubDeportivoApp.Datos
 {
@@ -21,6 +22,14 @@ namespace ClubDeportivoApp.Datos
                 MySqlCommand comando = new MySqlCommand("NuevoSocio", mySqlConnection);
                 comando.CommandType = CommandType.StoredProcedure;
                 comando.Parameters.Add("CodSocio", MySqlDbType.VarChar).Value = socio.CodSocio;
+                comando.Parameters.Add("Nombre", MySqlDbType.VarChar).Value = socio.Nombre;
+                comando.Parameters.Add("Apellido", MySqlDbType.VarChar).Value = socio.Apellido;
+                comando.Parameters.Add("Dni", MySqlDbType.Int32).Value = socio.Dni;
+                comando.Parameters.Add("FechaNac", MySqlDbType.DateTime).Value = socio.FechaNac;
+                comando.Parameters.Add("Direccion", MySqlDbType.VarChar).Value = socio.Direccion;
+                comando.Parameters.Add("Telefono", MySqlDbType.VarChar).Value = socio.Telefono;
+                comando.Parameters.Add("Email", MySqlDbType.VarChar).Value = socio.Email;
+                comando.Parameters.Add("FichaMedica", MySqlDbType.Bit).Value = socio.FichaMedica;
                 comando.Parameters.Add("Carnet", MySqlDbType.Bit).Value = socio.Carnet;
                 comando.Parameters.Add("FechaInscripcion", MySqlDbType.VarChar).Value = socio.FechaInscripcion;
                 comando.Parameters.Add("Moroso", MySqlDbType.Bit).Value = socio.Moroso;
@@ -47,11 +56,5 @@ namespace ClubDeportivoApp.Datos
             }
             return salida;
         }
-    } 
-}
-
-
-
-
-}
+    }
 }
