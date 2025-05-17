fc58a4041c0abb5398ca85d3a1be3ba5fb81ae7c
Guille Benitez <guillehbenitez@gmail.com>
2025-05-17 15:09:46 -0300

    Se corrigieron pk y fk en cuota diaria y cuota mensual

--- ClubDeportivoApp/SQL/Tabla Cuota Diaria.sql
+++ ClubDeportivoApp/SQL/Tabla Cuota Diaria.sql
@@ -3,9 +3,11 @@ USE clubdeportivo;
 DROP TABLE IF EXISTS CuotaDiaria;
 
 CREATE TABLE CuotaDiaria (
-	CodCuota VARCHAR(50) PRIMARY KEY,
+	CodCuota VARCHAR(50),
 	ValorFinal FLOAT NOT NULL,
-	TipoDePago VARCHAR(50) NOT NULL
+	TipoDePago VARCHAR(50) NOT NULL,
+	CodNoSocio VARCHAR(50) NOT NULL,
+	constraint pk_CuotaDiaria primary key (CodCuota),
+	constraint fk_CodNoSocio FOREIGN KEY (CodNoSocio) REFERENCES NoSocios(CodNoSocio)
 );
 
-constraint pk_CuotaDiaria primary key (CodCuota),
