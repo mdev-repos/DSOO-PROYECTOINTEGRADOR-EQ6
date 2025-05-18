07589292ac48cf00f17f165db0b3a24dd501f6f8
Guille Benitez <guillehbenitez@gmail.com>
2025-05-18 18:51:29 -0300

    Corrección tablas cuota diaria y cuota mensual

--- ClubDeportivoApp/SQL/Tabla Cuota Diaria.sql
+++ ClubDeportivoApp/SQL/Tabla Cuota Diaria.sql
@@ -3,11 +3,11 @@ USE clubdeportivo;
 DROP TABLE IF EXISTS CuotaDiaria;
 
 CREATE TABLE CuotaDiaria (
-	CodCuota VARCHAR(50),
+	CodCuotaDiaria VARCHAR(50),
 	ValorFinal FLOAT NOT NULL,
 	TipoDePago VARCHAR(50) NOT NULL,
 	CodNoSocio VARCHAR(50) NOT NULL,
-	constraint pk_CuotaDiaria primary key (CodCuota),
+	constraint pk_CuotaDiaria primary key (CodCuotaDiaria),
 	constraint fk_CodNoSocio FOREIGN KEY (CodNoSocio) REFERENCES NoSocios(CodNoSocio)
 );
 
